a69136c43a61bd071c7d659a7cb9b7a2c838c193
Ale David Weber <Alexis.Weber@mailfence.com>
2024-08-12 15:58:46 -0300

    add users dataflow and reapir env, config,  errors

--- src/parse/content_files.rs
+++ src/parse/content_files.rs
@@ -4,14 +4,18 @@ pub fn parse_env(
     master_key: &str,
     port: &str,
     server_url: &str,
+    project_name: &str,
 ) -> String {
     let env = format!(
-        "DATABASe_URL = {}
+"DATABASE_URL = {}
 APPLICATION_ID = {}
 MASTER_KEY = {}
 HTTP_PORT = {}
-SERVER_URL = {}",
-        url, app_id, master_key, port, server_url
+SERVER_URL = {}
+APP_NAME = {}
+CLOUD_PATH = './build/cloud/main.js'
+",
+        url, app_id, master_key, port, server_url, project_name
     );
 
     env
@@ -25,11 +29,13 @@ pub fn index_content() -> String {
         import { parseServer } from './parseServer';
         import cors from 'cors';
         import http from 'node:http';
+        import config from './config';
         
         
         export const app = express();
         
         Parse.initialize(config.APPLICATION_ID, config.MASTER_KEY);
+        Parse.serverURL = config.SERVER_URL;
         
         
         export const httpServer = http.createServer(app)
@@ -128,15 +134,311 @@ pub fn config_content() -> String {
             desc: 'Referenece to your server URL. Replace this when your app is hosted',
             devDefault: 'http://localhost:1337/server',
          }),
-         
-        }
+         DATABASE_URL: str({
+         desc: 'url database'
+         }),
+         APP_NAME: str({
+             desc: 'app name'
+         }),
+         CLOUD_PATH: str({
+             desc: 'path cloud main'
+         })
+        })
         ",
     );
     config_ts
 }
 
 pub fn parse_server_content() -> String {
-    let parse_server = format!("parse");
+    let parse_server = String::from(
+        r#"
+        // @ts-ignore
+        import { ParseServer } from 'parse-server';
+        import config from './config';
+        
+        const dataBaseUri =  config.DATABASE_URL;
+        
+        const serverPublicUrl = config.SERVER_URL;
+        
+        export const parseServer = new ParseServer({
+          liveQuery: {
+            classNames: ["ItemsMinted", "CollectionsPolygon"]
+          },
+          websocketTimeout: 60 * 1000,
+        
+          databaseURI: dataBaseUri,
+          cloud: config.CLOUD_PATH,
+          serverURL: config.SERVER_URL,
+          logsFolder: './logs',
+          publicServerURL: serverPublicUrl,
+          appName: config.APP_NAME,
+          appId: config.APPLICATION_ID,
+          masterKey: config.MASTER_KEY,
+          allowClientClassCreation: true,
+          verifyUserEmails: false,
+          emailVerifyTokenValidityDuration: 2 * 60 * 60,
+        });
+        
+        (() => {
+          parseServer.start();
+        })();
+        
+        "#,
+    );
 
     parse_server
+}
+
+pub fn tsconfig_content() -> String {
+    let tsconfig = String::from(
+        r#"
+        {
+          "compilerOptions": {
+            "strict": true,
+            "module": "commonjs",
+            "outDir": "build",
+            "declaration": true,
+            "declarationDir": "build",
+            "sourceMap": true,
+            "target": "es2017",
+            "rootDir": ".",
+            "esModuleInterop": true,
+            "skipLibCheck": true,
+            "allowJs": true
+          },
+          "include": ["src", "cloud"],
+          "files": ["src/global.d.ts"]
+        }
+
+        "#,
+    );
+
+    tsconfig
+}
+
+pub fn nodemon_content() -> String {
+    let nodemon_json = String::from(
+        r#"{
+      "watch": ["src", "cloud"],
+      "ext": "ts js",
+      "exec": "yarn build && ts-node ./src/index.ts"
+    }"#,
+    );
+
+    nodemon_json
+}
+
+pub fn main_content() -> String {
+    let main_js = String::from("require ('./routes/index.routes')");
+
+    main_js
+}
+
+pub fn routs_content() -> String {
+    let  index_routes_js = String::from(
+        r#"
+        const path = require('path');
+        const fs = require('fs');
+        
+        const PATH_NAME = path.join(__dirname, '../modules/');
+        
+        fs.readdirSync(PATH_NAME).forEach((folder) => {
+          const folderPath = path.join(PATH_NAME, folder);
+          const files = fs.readdirSync(folderPath);
+          files.forEach((file) => {
+            if (file.endsWith('Clouds.js')) {
+              const filePath = path.join(folderPath, file);
+              require(filePath);
+            }
+          });
+        });
+        "#,
+    );
+
+    index_routes_js
+}
+
+pub fn user_clouds_content() -> String {
+    let user_clouds_js = String::from(r#"
+        import { createUserNewControllers, deleteUserControllers, getAllUsersControllers, getUserByIdControllers, updateUserControllers } from './userControllers';
+        
+        
+        Parse.Cloud.define('getAllUsers', getAllUsersControllers(Parse));
+        Parse.Cloud.define('getUserById', getUserByIdControllers(Parse), { requireUser: true });
+        Parse.Cloud.define('createUser', createUserNewControllers(Parse));
+        Parse.Cloud.define('deleteUser', deleteUserControllers(Parse), { requireUser: true });
+        Parse.Cloud.define('updateUser', updateUserControllers(Parse), { requireUser: true });"#);
+    
+    user_clouds_js
+}
+
+pub fn user_controller_content() -> String {
+    let user_controller_js = String::from (r#"
+        import { 
+          createNewUserServices, 
+          deleteUserServices, 
+          getAllUsersServices, 
+          getUserByIdServices, 
+          updateUserServices 
+        } from "./userServices";
+        
+        const handleRequest = (serviceFunction) => {
+          return async (request) => {
+            try {
+              const result = await serviceFunction(request.params, request.headers);
+              return {
+                
+                status_result: "Ok",
+                ...result,
+              };
+            } catch (error) {
+              console.error(`Error code: ${error.code}, Error message: ${error.message}`);
+              return {
+                status: 'error',
+                result: false,
+                errorDetails: {
+                  code: error.code || Parse.Error.INTERNAL_SERVER_ERROR,
+                  message: error.message,
+                },
+              };
+            }
+          };
+        };
+        
+        export function getAllUsersControllers(Parse) {
+          return handleRequest(async ({ page }) => {
+            const users = await getAllUsersServices(page);
+            return { users };
+          });
+        }
+        
+        export function getUserByIdControllers(Parse) {
+          return handleRequest(async ({ userId }) => {
+            const user = await getUserByIdServices(userId);
+            return { user };
+          });
+        }
+        
+        export function createUserNewControllers(Parse) {
+          return handleRequest(async ({ objectData }) => {
+            const user = await createNewUserServices(objectData);
+            return { user };
+          });
+        }
+        
+        export function updateUserControllers(Parse) {
+          return handleRequest(async ({ userId, objectData }) => {
+            const user = await updateUserServices(userId, objectData);
+            return { user };
+          });
+        }
+        
+        export function deleteUserControllers(Parse) {
+          return handleRequest(async ({ userId }, headers) => {
+            const sessionToken = headers['x-parse-session-token'];
+            await deleteUserServices(userId, sessionToken);
+            return { message: 'User deleted successfully' };
+          });
+        }
+        "#);
+    
+    user_controller_js
+}
+
+pub fn user_srvices_content() -> String {
+    let user_services_js = String::from(r#"
+        const User = Parse.Object.extend('User');
+        
+        // Helper function to handle errors
+        function throwError(errorCode, errorMessage) {
+          throw new Parse.Error(errorCode, errorMessage);
+        }
+        
+        // Helper function to fetch a user by ID
+        async function fetchUserById(userId) {
+          const query = new Parse.Query(User);
+          query.equalTo('objectId', userId);
+          return await query.first({ useMasterKey: true });
+        }
+        
+        export async function getAllUsersServices(page) {
+          if (page === null || page === undefined) {
+            throwError(Parse.Error.INVALID_QUERY, 'Page number is missing.');
+          }
+        
+          const query = new Parse.Query(User);
+          const users = await query.find({ useMasterKey: true });
+        
+          if (!users || users.length === 0) {
+            throwError(Parse.Error.OBJECT_NOT_FOUND, 'No users found.');
+          }
+        
+          return users;
+        }
+        
+        export async function getUserByIdServices(userId) {
+          if (!userId) {
+            throwError(Parse.Error.INVALID_QUERY, 'User ID is missing.');
+          }
+        
+          const user = await fetchUserById(userId);
+          if (!user) {
+            throwError(Parse.Error.OBJECT_NOT_FOUND, `User with ID ${userId} does not exist.`);
+          }
+        
+          return user;
+        }
+        
+        export async function createNewUserServices(objectData) {
+          if (!objectData) {
+            throwError(Parse.Error.OBJECT_NOT_FOUND, 'objectData is missing.');
+          }
+        
+          const user = new User();
+          Object.keys(objectData).forEach(key => {
+            if (objectData.hasOwnProperty(key)) {
+              user.set(key, objectData[key]);
+            }
+          });
+        
+          await user.save();
+          return user;
+        }
+        
+        export async function updateUserServices(userId, objectData) {
+          if (!userId || !objectData) {
+            throwError(Parse.Error.INVALID_QUERY, 'User ID or objectData is missing.');
+          }
+        
+          const user = await fetchUserById(userId);
+          if (!user) {
+            throwError(Parse.Error.OBJECT_NOT_FOUND, `User with ID ${userId} does not exist.`);
+          }
+        
+          const nonUpdatableFields = ['username', 'email', 'password'];
+          Object.keys(objectData).forEach(key => {
+            if (!nonUpdatableFields.includes(key)) {
+              user.set(key, objectData[key]);
+            }
+          });
+        
+          await user.save(null, { useMasterKey: true });
+          return user;
+        }
+        
+        export async function deleteUserServices(userId) {
+          if (!userId) {
+            throwError(Parse.Error.INVALID_QUERY, 'User ID is missing.');
+          }
+        
+          const user = await fetchUserById(userId);
+          if (!user) {
+            throwError(Parse.Error.OBJECT_NOT_FOUND, `User with ID ${userId} does not exist.`);
+          }
+        
+          await user.destroy({ useMasterKey: true });
+        }
+
+        "#);
+    user_services_js
 }
--- src/parse/files_create.rs
+++ src/parse/files_create.rs
@@ -1,4 +1,7 @@
-use super::content_files::{config_content, index_content, package_content, parse_server_content};
+use super::content_files::{
+    config_content, index_content, main_content, nodemon_content, package_content,
+    parse_server_content, routs_content, tsconfig_content, user_clouds_content, user_controller_content, user_srvices_content,
+};
 
 use std::io::Error;
 use std::{fs::File, io::Write};
@@ -48,12 +51,84 @@ fn global_d_ts(project_name: &str) -> Result<(), Error> {
     Ok(())
 }
 
+fn tsconfig_json(project_name: &str) -> Result<(), Error> {
+    let content = tsconfig_content();
+
+    let mut tsconfig_json = File::create(format!("{}/tsconfig.json", project_name))?;
+    tsconfig_json.write_all(content.as_bytes())?;
+
+    Ok(())
+}
+
+fn nodemon_json(project_name: &str) -> Result<(), Error> {
+    let content = nodemon_content();
+
+    let mut nodemon_json = File::create(format!("{}/nodemon.json", project_name))?;
+    nodemon_json.write_all(content.as_bytes())?;
+
+    Ok(())
+}
+
+fn main_js(project_name: &str) -> Result<(), Error> {
+    let content = main_content();
+
+    let mut main_js = File::create(format!("{}/clouds/main.js", project_name))?;
+    main_js.write_all(content.as_bytes())?;
+
+    Ok(())
+}
+
+fn index_routs_js(project_name: &str) -> Result<(), Error> {
+    let content = routs_content();
+
+    let mut routes_index_js =
+        File::create(format!("{}/clouds/routes/index.routes.js", project_name))?;
+    routes_index_js.write_all(content.as_bytes())?;
+
+    Ok(())
+}
+
+fn user_clouds_js (project_name: &str) -> Result <(), Error> {
+    let content = user_clouds_content();
+    
+    let mut user_clouds_js = File::create(format!("{}/clouds/modules/user/userClouds.js", project_name))?;
+    
+    user_clouds_js.write_all(content.as_bytes())?;    Ok(())
+}
+
+fn user_controller_js(project_name: &str) -> Result<(),Error> {
+    let content = user_controller_content();
+    
+    let mut user_controller_js = File::create(format!("{}/clouds/modules/user/userControllers.js", project_name))?;
+    
+user_controller_js.write_all(content.as_bytes())?;
+
+Ok(())
+}
+
+fn user_service_js (project_name: &str) -> Result<(),Error> {
+    let content = user_srvices_content();
+    
+    let mut user_service_js = File::create(format!("{}/clouds/modules/user/userServices.js", project_name))?;
+    
+    user_service_js.write_all(content.as_bytes())?;
+    
+    Ok(())
+}
+
 pub fn create_files(project_name: &str) -> Result<(), Error> {
     index_ts(project_name)?;
     package_json(project_name)?;
     parse_server_ts(project_name)?;
     config_ts(project_name)?;
     global_d_ts(project_name)?;
-
+    tsconfig_json(project_name)?;
+    nodemon_json(project_name)?;
+    main_js(project_name)?;
+    index_routs_js(project_name)?;
+    user_clouds_js(project_name)?;
+    user_controller_js(project_name)?;
+    user_service_js(project_name)?;
+    
     Ok(())
 }
